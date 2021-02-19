942af4398fb3b628d095ae326ab897d4ef0770f2
viruuzz <79336127+viruuzz@users.noreply.github.com>
2021-02-19 22:32:41 +0100

    Add files via upload

---
+++ Tool.bat
@@ -0,0 +1,38 @@
+@echo off
+TITLE ViruzzTool
+echo Tool by Viruzz
+color 02
+set /p discord="Discord: "
+cls
+color 02
+set /p packet="Paquet (1-500): "
+cls
+set /p time="Time (0-3500): "
+cls
+color 02
+echo 10%% attack
+timeout 1 > NUL
+echo 20%% attack
+timeout 1 > NUL
+echo 30%% attack
+timeout 1 > NUL
+echo 40%% attack
+timeout 1 > NUL
+echo 50%% attack
+timeout 1 > NUL
+echo 60%% attack
+timeout 1 > NUL
+echo 70%% attack
+timeout 1 > NUL
+echo 80%% attack
+timeout 1 > NUL
+echo 90%% attack
+timeout 1 > NUL
+echo 100%% attack
+timeout 1 > NUL
+echo Successfully inject to %discord%
+timeout 2 > NUL
+cls
+echo Paquet: %packet% paqt
+echo Time: %time% 
+pause 
